008540444505bf53acd0d098ba05fcf3b7bc0a39
Yaskata23 <yasen.s.dimchov.2021@elsys-bg.org>
2023-04-16 21:10:38 +0300

    added player

--- settings.py
+++ settings.py
@@ -1,3 +1,12 @@
+import math
+
 # game settings
 RES = WIDTH, HEIGHT = 1600, 900
-FPS = 60
+FPS = 0
+
+PLAYER_POS = 1.5, 5
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
+
+FOV = math.pi / 3
